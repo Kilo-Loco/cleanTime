17d8654e55e7ea4b167cceb657770a8edd67e80d
Kyle Lee <kiloloco@me.com>
2016-01-06 21:32:07 -0800

    Added shadows and rounded corners for content views

---
+++ Clean Time/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Clean Time
+//
+//  Created by Kyle Lee on 1/6/16.
+//  Copyright © 2016 Kilo Loco. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_COLOR: CGFloat = 167.0 / 255.0
